6f45c9eae5fc3a2fc7c1ab3971d8ce19ced2c9d2
Mikko Ohtamaa <mikko@opensourcehacker.com>
2014-12-29 19:08:36 +1100

    Fixed some issues with the test script

--- cryptoassets/core/tests/setup-testing-droneio.sh
+++ cryptoassets/core/tests/setup-testing-droneio.sh
@@ -19,12 +19,18 @@ pip install --quiet -r test-extra-requirements.txt --use-mirrors
 
 # Make sure we have PSQL test database for conflict resolver test case
 # http://docs.drone.io/databases.html
-psql -c 'create database unittest-conflict-resolution;' -U postgres
+psql -c 'create database "unittest-conflict-resolution";' -U postgres
 
 # Build SSH tunnelling to the bitcoind server
+# First grab SSH key from drone.io config so it's not visible in build log
+echo "" > /tmp/private-key
+for i in $(echo $SSH_PRIV_KEY | tr "," "\n")
+do
+    echo $i >> /tmp/private-key
+done
 eval `ssh-agent`
-ssh-add -i /tmp/private-key
-ssh -f -C $CHECKOUT_HOME/cryptoassets/core/tests/droneio-ssh-config bitcoind-test-server
+ssh-add /tmp/private-key
+ssh -f -F $CHECKOUT_HOME/cryptoassets/core/tests/droneio-ssh-config bitcoind-test-server
 
 # Run tests using py.test test runner
 py.test cryptoassets
