31173ecbe2897e9840f43c8042b85132821935e1
Mikko Ohtamaa <mikko@opensourcehacker.com>
2014-12-30 22:54:45 +1100

    More CI work

--- cryptoassets/core/tests/setup-testing-droneio.sh
+++ cryptoassets/core/tests/setup-testing-droneio.sh
@@ -1,16 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Setup test running for Ubuntu 12.02 / Drone IO
 #
+
+set -e
 
 CHECKOUT_HOME=/home/ubuntu/src/bitbucket.org/miohtama/cryptoassets
 
 # Need to upgrade to Python 3.4
 sudo add-apt-repository ppa:fkrull/deadsnakes 2>&1 > /dev/null
-sudo apt-get -qq update; sudo apt-get -qq install python3.4-dev
+sudo apt-get -qq update 2>&1 > /dev/null
+sudo apt-get -qq install python3.4-dev 2>&1 > /dev/null
 
 python3.4 -m venv venv
-source venv/bin/activate
+. venv/bin/activate
 
 # Make sure pip itself is up to date
 pip install -U --quiet pip
@@ -19,7 +22,7 @@ pip install --quiet -r test-extra-requirements.txt --use-mirrors
 
 # Make sure we have PSQL test database for conflict resolver test case
 # http://docs.drone.io/databases.html
-psql -c 'create database "unittest-conflict-resolution";' -U postgres
+psql -c 'CREATE DATABASE IF NOT EXISTS "unittest-conflict-resolution";' -U postgres
 
 # Build SSH tunnelling to the bitcoind server
 # First grab SSH key from drone.io config so it's not visible in build log
@@ -29,9 +32,7 @@ do
     echo $i >> /tmp/private-key
 done
 chmod o-wrx,g-rwx /tmp/private-key
-eval `ssh-agent`
-ssh-add /tmp/private-key
-ssh -vvv -N -f -F $CHECKOUT_HOME/cryptoassets/core/tests/droneio-ssh-config bitcoind-test-server
+ssh -vvv -N -f -F $CHECKOUT_HOME/cryptoassets/core/tests/droneio-ssh-config $BITCOIND_SERVER
 
 # Run tests using py.test test runner
 ls venv/bin  # debug
